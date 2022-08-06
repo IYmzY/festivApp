2fca14a2bc0c8d6a7fbb8780ea7fd0e857254b67
YmzY <redanabil2199@gmail.com>
2022-08-06 18:41:17 +0200

    init

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyB0nU53fshyIpRakNb9GeTGrx0JsbWcmbI',
+    authDomain: 'my-festival-app.firebaseapp.com',
+    projectId: 'my-festival-app',
+    storageBucket: 'my-festival-app.appspot.com',
+    messagingSenderId: '170240173140',
+    appId: '1:170240173140:web:e628a6b9578b9be79c39c1',
+    measurementId: 'G-7ELJFHZ9KW',
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig: {
+    apiKey: 'AIzaSyB0nU53fshyIpRakNb9GeTGrx0JsbWcmbI',
+    authDomain: 'my-festival-app.firebaseapp.com',
+    projectId: 'my-festival-app',
+    storageBucket: 'my-festival-app.appspot.com',
+    messagingSenderId: '170240173140',
+    appId: '1:170240173140:web:e628a6b9578b9be79c39c1',
+    measurementId: 'G-7ELJFHZ9KW',
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
